1a14c4a506d45a1da001e2c428897909775e5f4a
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:05:36 +0530

    Fetched

---
+++ UC3_EmployeeType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class UC3_EmployeeType
+    {
+        public void fullTime_partTime()
+        {
+            int dailyWagesPerHr = 20;
+            int workingHr;
+            int fullTime = 1;
+            Random rand = new Random();
+            int EmpType = rand.Next(0, 2);
+            if (EmpType == fullTime)
+            {
+                workingHr = 8;
+                Console.WriteLine("FullTime Employee");
+                Console.WriteLine("Daily wages " + workingHr * dailyWagesPerHr);
+            }
+            else
+            {
+                workingHr = 4;
+                Console.WriteLine("Part Time Employee");
+                Console.WriteLine("Daily wages " + workingHr * dailyWagesPerHr);
+            }
+        }
+    }
+}
