d7e5f8e5f7cd5522fcffb2bbeaa63ff440c99551
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:07:31 +0530

    Fetched

---
+++ UC4_SwitchCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class UC4_SwitchCase
+    {
+        public void Emp_Type()
+        {
+            int dailyWagesPerHr = 20;
+            int workingHr;
+            Random rand = new Random();
+            int EmpType=rand.Next(3);
+            switch (EmpType)
+            {
+                case 1:
+                    workingHr = 8;
+                    Console.WriteLine("Fulltime employee getting salary per day : " + workingHr*dailyWagesPerHr );
+                    break;
+                case 2:
+                    workingHr = 4;
+                    Console.WriteLine("Parttime employee getting salary per day: " + workingHr*dailyWagesPerHr );
+                    break;
+                default:
+                    Console.WriteLine("Not a Employee");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
