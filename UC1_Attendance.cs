dccb6b54eb72b3bb673ad00038cfaffe0c200dc1
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:00:09 +0530

    Fetched

---
+++ UC1_Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class UC1_Attendance
+    {
+        public void Attendance()
+        {
+            Random rand = new Random();
+            int emp_Check = rand.Next(2);
+            switch (emp_Check)
+            {
+                case 1:
+                    Console.WriteLine("Employee is present");
+                    break;
+                case 0:
+                    Console.WriteLine("Employee is absent");
+                    break;
+                default:
+                    Console.WriteLine("Holiday");
+                    break;
+            }
+        }  
+    }
+}
