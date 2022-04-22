cde62516125fde1b785b4481161f27dce802ec31
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:04:13 +0530

    Fetched

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            UC5_MonthlySalary u = new UC5_MonthlySalary();
+            u.monthSal ();
+
+        }
+    }
+}
