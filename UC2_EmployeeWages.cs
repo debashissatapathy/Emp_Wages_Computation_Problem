8716bcf094409e916a571d9e932e09c101cd0058
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:02:14 +0530

    Fetched

---
+++ UC2_EmployeeWages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class UC2_EmployeeWages
+    {
+        public void EmpWages()
+        {
+            int WagePerHr = 20;
+            int EmpHr = 8;
+            int DailyWages = WagePerHr * EmpHr;
+            Console.WriteLine("Daily Salary is " + DailyWages);
+
+
+
+        }
+    }
+}
