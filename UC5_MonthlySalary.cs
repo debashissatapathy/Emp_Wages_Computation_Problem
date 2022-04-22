6596c8c3192210cd321a10fa760c70669344b571
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:10:25 +0530

    fetched

---
+++ UC5_MonthlySalary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wages_Computation_Problem
+{
+    class UC5_MonthlySalary
+    {
+        public void monthSal()
+        {
+            int dailyWagesPerHr = 20;
+            int workingHr;
+            int workingDays = 20;            
+            Random rand = new Random();
+            int EmpType=rand.Next(3);
+            switch (EmpType)
+            {
+                case 1:
+                    workingHr = 8;
+                    Console.WriteLine("Fulltime employee getting salary per day : " + workingHr*dailyWagesPerHr );
+                    Console.WriteLine("Fulltime employee getting monthly salary : " + workingHr * dailyWagesPerHr*workingDays);
+                    break;
+                case 2:
+                    workingHr = 4;
+                    Console.WriteLine("Parttime employee getting salary per day: " + workingHr*dailyWagesPerHr );
+                    Console.WriteLine("Parttime employee getting monthly salary: " + workingHr * dailyWagesPerHr * workingDays);
+                    break;
+                default:
+                    Console.WriteLine("Not a Employee");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
